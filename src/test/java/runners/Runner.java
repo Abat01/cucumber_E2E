480f6aa9a74eb0e5cb95ba2fb0ae14b01814162d
techproed <techproeducation@gmail.com>
2023-03-28 20:53:21 +0300

    End2End

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,21 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions (
+
+        features="src/test/resources/features",
+        glue= {"stepdefinitions", "hooks"},
+        tags="@amazon_search"  ,
+
+        dryRun= false
+
+)
+
+
+public class Runner {
+
+}
